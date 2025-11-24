04a66d41dc2f8b6127bba079aa40999e33977a1c
Umarfarooq J Ron <umarron.10@gmail.com>
2025-11-24 16:21:35 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,17 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+if _name_ == "_main_":
+    # Ensure correct number of arguments
+    if len(sys.argv) != 3:
+        print("Usage: python add.py <num1> <num2>")
+        sys.exit(1)
+
+    # Convert arguments to integers
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+
+    # Print the sum
+    print("Sum:", add(x, y))
